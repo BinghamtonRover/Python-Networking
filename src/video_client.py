5729b3fcab09affb67fc342db56411c82162ee07
David-Lovejoy <98231301+David-Lovejoy@users.noreply.github.com>
2023-02-12 14:43:27 -0500

    remvod client and server

--- src/video_client.py
+++ src/video_client.py
@@ -3,6 +3,6 @@ from .udp_client import UdpClient
 from lib.network.generated.Protobuf.wrapper_pb2 import WrappedMessage
 
 class VideoClient(UdpClient):
-	def send_message(self, id, frame, address, port):
+	def send_frame(self, id, frame, address, port):
 		wrapped_message = WrappedMessage(name=id, data=frame)
 		self.send(address=address, port=port, data=wrapped_message.SerializeToString())
