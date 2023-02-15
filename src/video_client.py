a3a093b1f387a51aa3229022b08040195d16a605
Levi Lesches <levilesches@gmail.com>
2023-02-14 19:51:50 -0500

    Fixed VideoClient

--- src/video_client.py
+++ src/video_client.py
@@ -1,8 +1,17 @@
-from .udp_client import UdpClient
+import cv2
 
-from lib.network.generated.Protobuf.wrapper_pb2 import WrappedMessage
+from .proto_client import ProtoClient
 
-class VideoClient(UdpClient):
-	def send_frame(self, id, frame, address=None, port=None):
-		wrapped_message = WrappedMessage(name=id, data=frame)
-		self.send(data=wrapped_message.SerializeToString(), address=address, port=port)
+from lib.network.generated.Protobuf.video_pb2 import *
+
+class VideoClient(ProtoClient):
+	def send_frame(self, name, frame, address=None, port=None):
+		if name not in CameraName.values(): 
+			raise TypeError("VideoClient.send_frame expects argument 1 to be a CameraName value")
+		frame = cv2.resize(frame, (500, 500))
+		encoded, buffer = cv2.imencode('.jpg', frame, [cv2.IMWRITE_JPEG_QUALITY, 80])
+		message = VideoFrame(
+			name=name,
+			frame=buffer.tobytes(),
+		)
+		self.send_message(message=message, address=address, port=port)
