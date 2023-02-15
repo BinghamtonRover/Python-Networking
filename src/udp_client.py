46d0550d6fa642966c1a5cef509f595ac6c8054f
Levi Lesches <levilesches@gmail.com>
2023-02-14 19:51:44 -0500

    Fixed UdpClient address/port

--- src/udp_client.py
+++ src/udp_client.py
@@ -4,12 +4,14 @@ class UdpClient:
 	def __init__(self, address=None, port=None, buffer_size=1024):
 		self.buffer_size = buffer_size
 		self.udp_client_socket = socket.socket(family = socket.AF_INET, type = socket.SOCK_DGRAM)
-		self.address = None
-		self.port = None
+		self.address = address
+		self.port = port
 
 	def send(self, data, address=None, port=None):
 		if (address is None): address = self.address
 		if (port is None): port = self.port
+		if (address is None or port is None):
+			raise ValueError("No port or address specified")
 		self.udp_client_socket.sendto(data, (address, port))
 
 	def close(self):
