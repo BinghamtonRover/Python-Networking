312de9480dc2bc8e2de0855a6095d188ccf3efde
Levi Lesches <levilesches@gmail.com>
2023-02-14 19:52:01 -0500

    Fixed examples

--- bin/video_client.py
+++ bin/video_client.py
@@ -1,9 +1,10 @@
 import time
 import cv2
 from lib.network import VideoClient
+from lib.network.generated.Protobuf.video_pb2 import *
 
-def init_camera():
-	camera = cv2.VideoCapture(1)
+def init_camera(id):
+	camera = cv2.VideoCapture(id)
 	if not camera.isOpened(): 
 		print("[Error] Cannot open camera")
 		quit()
@@ -17,11 +18,18 @@ def read_frame(camera):
 	return frame
 
 if __name__ == '__main__':
-	camera = init_camera()
+	print("Initializing...")
+	camera = init_camera(0)
+	cam2 = init_camera(1)
+	cam3 = init_camera(2)
 	client = VideoClient(address="localhost", port=8001)
 	while True: 
-		frame = read_frame(camera)
-		client.send_frame("Video demo", frame)
+		frame1 = read_frame(camera)
+		client.send_frame(CameraName.CAMERA_NAME_ROVER_FRONT, frame1)
+		frame2 = read_frame(cam2)
+		client.send_frame(CameraName.CAMERA_NAME_ROVER_REAR, frame2)
+		frame3 = read_frame(cam3)
+		client.send_frame(CameraName.CAMERA_NAME_ARM_BASE, frame3)
 		print("Sent frame")
 		time.sleep(1/24)
 	camera.release()
