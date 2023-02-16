ba1e0d36e4ab8daabc3207096bc928e0c3d45da3
Levi Lesches <levilesches@gmail.com>
2023-02-16 00:24:08 -0500

    On loop (#5)
    Added `UdpServer.on_loop` to allow code to run periodically, and `UdpServer.buffer` to allow easy control over the buffer size.

--- src/udp_server.py
+++ src/udp_server.py
@@ -1,10 +1,9 @@
 import socket
 
-BUFFER_SIZE = 1024
-
 class UdpServer:
-	def __init__(self, port):
+	def __init__(self, port, buffer=1024):
 		self.port = port
+		self.buffer = buffer
 		self.socket = socket.socket(family = socket.AF_INET, type = socket.SOCK_DGRAM)
 		self.socket.bind(("0.0.0.0", port))
 		self.socket.settimeout(0.1)
@@ -13,10 +12,14 @@ class UdpServer:
 		print(f"Server started on port {self.port}")
 		try:
 			while(True):
-				try: message, source = self.socket.recvfrom(BUFFER_SIZE)
+				try: message, source = self.socket.recvfrom(self.buffer)
 				except socket.timeout: continue
 				else: self.on_data(message, source)
+				finally: self.on_loop()
 		except KeyboardInterrupt: self.close()
+
+	# Override this to insert your own logic in between waiting
+	def on_loop(self): pass
 
 	def on_data(self, data, source): 
 		print(f"Received message from {source}: {data}")
