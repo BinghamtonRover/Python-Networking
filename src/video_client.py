0a319c8f9d2330edeb9db7c42b3da72178074505
jmcmah10 <jmcmah10@binghamton.edu>
2023-02-28 07:06:42 +0000

    Video fixes

--- src/video_client.py
+++ src/video_client.py
@@ -6,7 +6,7 @@ from lib.network.generated.Protobuf.video_pb2 import *
 
 class VideoClient(ProtoClient):
 	def send_frame(self, name, frame, address=None, port=None):
-		encoded, buffer = cv2.imencode('.jpg', frame, [cv2.IMWRITE_JPEG_QUALITY, 80])
+		encoded, buffer = cv2.imencode('.jpg', frame, [cv2.IMWRITE_JPEG_QUALITY, 70])
 		message = VideoFrame(
 			name=name,
 			frame=buffer.tobytes(),
