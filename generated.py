32d3766c399102f6d91219af91acb257ebbc3536
Levi Lesches <levilesches@gmail.com>
2023-05-04 05:13:15 -0400

    Updated Protobuf with MARS data

--- generated.py
+++ generated.py
@@ -3,4 +3,4 @@ from network.src.generated.core_pb2 import *
 from network.src.generated.drive_pb2 import *
 from network.src.generated.video_pb2 import *
 from network.src.generated.wrapper_pb2 import *
-
+from network.src.generated.mars_pb2 import *
