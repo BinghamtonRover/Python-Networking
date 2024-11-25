d84e9c6b6e098ad9bb01ada3f304d80bb013f588
Levi Lesches <levilesches@gmail.com>
2024-11-24 22:06:51 -0500

    Updated Protobuf and regenerated

--- src/generated/gps_pb2.py
+++ src/generated/gps_pb2.py
@@ -11,20 +11,20 @@ from google.protobuf.internal import builder as _builder
 _sym_db = _symbol_database.Default()
 
 
+import version_pb2 as version__pb2
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\tgps.proto\"G\n\x0eGpsCoordinates\x12\x10\n\x08latitude\x18\x01 \x01(\x02\x12\x11\n\tlongitude\x18\x02 \x01(\x02\x12\x10\n\x08\x61ltitude\x18\x03 \x01(\x02\".\n\x0bOrientation\x12\t\n\x01x\x18\x01 \x01(\x02\x12\t\n\x01y\x18\x02 \x01(\x02\x12\t\n\x01z\x18\x03 \x01(\x02\"P\n\rRoverPosition\x12\x1c\n\x03gps\x18\x01 \x01(\x0b\x32\x0f.GpsCoordinates\x12!\n\x0borientation\x18\x02 \x01(\x0b\x32\x0c.Orientationb\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\tgps.proto\x1a\rversion.proto\"G\n\x0eGpsCoordinates\x12\x10\n\x08latitude\x18\x01 \x01(\x02\x12\x11\n\tlongitude\x18\x02 \x01(\x02\x12\x10\n\x08\x61ltitude\x18\x03 \x01(\x02\".\n\x0bOrientation\x12\t\n\x01x\x18\x01 \x01(\x02\x12\t\n\x01y\x18\x02 \x01(\x02\x12\t\n\x01z\x18\x03 \x01(\x02\"k\n\rRoverPosition\x12\x1c\n\x03gps\x18\x01 \x01(\x0b\x32\x0f.GpsCoordinates\x12!\n\x0borientation\x18\x02 \x01(\x0b\x32\x0c.Orientation\x12\x19\n\x07version\x18\x03 \x01(\x0b\x32\x08.Versionb\x06proto3')
 
 _globals = globals()
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, _globals)
 _builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, 'gps_pb2', _globals)
 if _descriptor._USE_C_DESCRIPTORS == False:
-
   DESCRIPTOR._options = None
-  _globals['_GPSCOORDINATES']._serialized_start=13
-  _globals['_GPSCOORDINATES']._serialized_end=84
-  _globals['_ORIENTATION']._serialized_start=86
-  _globals['_ORIENTATION']._serialized_end=132
-  _globals['_ROVERPOSITION']._serialized_start=134
-  _globals['_ROVERPOSITION']._serialized_end=214
+  _globals['_GPSCOORDINATES']._serialized_start=28
+  _globals['_GPSCOORDINATES']._serialized_end=99
+  _globals['_ORIENTATION']._serialized_start=101
+  _globals['_ORIENTATION']._serialized_end=147
+  _globals['_ROVERPOSITION']._serialized_start=149
+  _globals['_ROVERPOSITION']._serialized_end=256
 # @@protoc_insertion_point(module_scope)
--- src/generated/video_pb2.py
+++ src/generated/video_pb2.py
@@ -11,24 +11,24 @@ from google.protobuf.internal import builder as _builder
 _sym_db = _symbol_database.Default()
 
 
+import version_pb2 as version__pb2
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x0bvideo.proto\"\x9c\x01\n\rCameraDetails\x12\x19\n\x04name\x18\x01 \x01(\x0e\x32\x0b.CameraName\x12\x18\n\x10resolution_width\x18\x02 \x01(\x05\x12\x19\n\x11resolution_height\x18\x03 \x01(\x05\x12\x0f\n\x07quality\x18\x04 \x01(\x05\x12\x0b\n\x03\x66ps\x18\x05 \x01(\x05\x12\x1d\n\x06status\x18\x06 \x01(\x0e\x32\r.CameraStatus\"G\n\tVideoData\x12\n\n\x02id\x18\x01 \x01(\t\x12\x1f\n\x07\x64\x65tails\x18\x02 \x01(\x0b\x32\x0e.CameraDetails\x12\r\n\x05\x66rame\x18\x03 \x01(\x0c\";\n\x0cVideoCommand\x12\n\n\x02id\x18\x01 \x01(\t\x12\x1f\n\x07\x64\x65tails\x18\x02 \x01(\x0b\x32\x0e.CameraDetails*\xc9\x01\n\x0c\x43\x61meraStatus\x12\x1b\n\x17\x43\x41MERA_STATUS_UNDEFINED\x10\x00\x12\x17\n\x13\x43\x41MERA_DISCONNECTED\x10\x01\x12\x12\n\x0e\x43\x41MERA_ENABLED\x10\x02\x12\x13\n\x0f\x43\x41MERA_DISABLED\x10\x03\x12\x19\n\x15\x43\x41MERA_NOT_RESPONDING\x10\x04\x12\x12\n\x0e\x43\x41MERA_LOADING\x10\x05\x12\x13\n\x0f\x46RAME_TOO_LARGE\x10\x06\x12\x16\n\x12\x43\x41MERA_HAS_NO_NAME\x10\x07*\x8c\x01\n\nCameraName\x12\x19\n\x15\x43\x41MERA_NAME_UNDEFINED\x10\x00\x12\x0f\n\x0bROVER_FRONT\x10\x01\x12\x0e\n\nROVER_REAR\x10\x02\x12\x12\n\x0e\x41UTONOMY_DEPTH\x10\x03\x12\x0e\n\nSUBSYSTEM1\x10\x04\x12\x0e\n\nSUBSYSTEM2\x10\x05\x12\x0e\n\nSUBSYSTEM3\x10\x06\x62\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x0bvideo.proto\x1a\rversion.proto\"\xe7\x01\n\rCameraDetails\x12\x19\n\x04name\x18\x01 \x01(\x0e\x32\x0b.CameraName\x12\x18\n\x10resolution_width\x18\x02 \x01(\x05\x12\x19\n\x11resolution_height\x18\x03 \x01(\x05\x12\x0f\n\x07quality\x18\x04 \x01(\x05\x12\x0b\n\x03\x66ps\x18\x05 \x01(\x05\x12\x1d\n\x06status\x18\x06 \x01(\x0e\x32\r.CameraStatus\x12\x11\n\tautofocus\x18\x07 \x01(\x08\x12\x0c\n\x04zoom\x18\x08 \x01(\x05\x12\x0b\n\x03pan\x18\t \x01(\x05\x12\x0c\n\x04tilt\x18\n \x01(\x05\x12\r\n\x05\x66ocus\x18\x0b \x01(\x05\"u\n\tVideoData\x12\n\n\x02id\x18\x01 \x01(\t\x12\x1f\n\x07\x64\x65tails\x18\x02 \x01(\x0b\x32\x0e.CameraDetails\x12\r\n\x05\x66rame\x18\x03 \x01(\x0c\x12\x19\n\x07version\x18\x04 \x01(\x0b\x32\x08.Version\x12\x11\n\timagePath\x18\x05 \x01(\t\"l\n\x0cVideoCommand\x12\n\n\x02id\x18\x01 \x01(\t\x12\x1f\n\x07\x64\x65tails\x18\x02 \x01(\x0b\x32\x0e.CameraDetails\x12\x19\n\x07version\x18\x03 \x01(\x0b\x32\x08.Version\x12\x14\n\x0ctakeSnapshot\x18\x04 \x01(\x08*\xc9\x01\n\x0c\x43\x61meraStatus\x12\x1b\n\x17\x43\x41MERA_STATUS_UNDEFINED\x10\x00\x12\x17\n\x13\x43\x41MERA_DISCONNECTED\x10\x01\x12\x12\n\x0e\x43\x41MERA_ENABLED\x10\x02\x12\x13\n\x0f\x43\x41MERA_DISABLED\x10\x03\x12\x19\n\x15\x43\x41MERA_NOT_RESPONDING\x10\x04\x12\x12\n\x0e\x43\x41MERA_LOADING\x10\x05\x12\x13\n\x0f\x46RAME_TOO_LARGE\x10\x06\x12\x16\n\x12\x43\x41MERA_HAS_NO_NAME\x10\x07*\xaf\x01\n\nCameraName\x12\x19\n\x15\x43\x41MERA_NAME_UNDEFINED\x10\x00\x12\x0f\n\x0bROVER_FRONT\x10\x01\x12\x0e\n\nROVER_REAR\x10\x02\x12\x12\n\x0e\x41UTONOMY_DEPTH\x10\x03\x12\x0e\n\nSUBSYSTEM1\x10\x04\x12\x0e\n\nSUBSYSTEM2\x10\x05\x12\x0e\n\nSUBSYSTEM3\x10\x06\x12\x0f\n\x0b\x42OTTOM_LEFT\x10\x07\x12\x10\n\x0c\x42OTTOM_RIGHT\x10\x08\x62\x06proto3')
 
 _globals = globals()
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, _globals)
 _builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, 'video_pb2', _globals)
 if _descriptor._USE_C_DESCRIPTORS == False:
-
   DESCRIPTOR._options = None
-  _globals['_CAMERASTATUS']._serialized_start=309
-  _globals['_CAMERASTATUS']._serialized_end=510
-  _globals['_CAMERANAME']._serialized_start=513
-  _globals['_CAMERANAME']._serialized_end=653
-  _globals['_CAMERADETAILS']._serialized_start=16
-  _globals['_CAMERADETAILS']._serialized_end=172
-  _globals['_VIDEODATA']._serialized_start=174
-  _globals['_VIDEODATA']._serialized_end=245
-  _globals['_VIDEOCOMMAND']._serialized_start=247
-  _globals['_VIDEOCOMMAND']._serialized_end=306
+  _globals['_CAMERASTATUS']._serialized_start=494
+  _globals['_CAMERASTATUS']._serialized_end=695
+  _globals['_CAMERANAME']._serialized_start=698
+  _globals['_CAMERANAME']._serialized_end=873
+  _globals['_CAMERADETAILS']._serialized_start=31
+  _globals['_CAMERADETAILS']._serialized_end=262
+  _globals['_VIDEODATA']._serialized_start=264
+  _globals['_VIDEODATA']._serialized_end=381
+  _globals['_VIDEOCOMMAND']._serialized_start=383
+  _globals['_VIDEOCOMMAND']._serialized_end=491
 # @@protoc_insertion_point(module_scope)
